45c24e79f6c328f941eafd2de2610a612d2d71c9
FSalinski <franek.salinski@gmail.com>
2022-11-23 18:08:03 +0100

    Cleaning01

--- Data cleaning and feature engineering/DataCleaning.py
+++ Data cleaning and feature engineering/DataCleaning.py
@@ -4,8 +4,13 @@ data = pd.read_csv('C:/Users/frane/kaggleproject/bank-data.csv', sep=';') # impo
 
 #  Dropping columns we decided to drop in EDA
 
-print(data.shape)
-data.drop(columns=['duration'], axis=1, inplace=True)
-print(data.shape)
+data.drop(columns=['duration', 'default'], axis=1, inplace=True)
 
-# Imputting missing data
+# Inputting missing data
+
+data['pdays'] = data['pdays'] < 999
+
+# pdays is True when a client was contacted in the previous campaign and False otherwise
+data.rename({'pdays':'contacted.in.previous'}, inplace=True)
+
+# Now we will get rid off 'unknown' values in categorical columns
