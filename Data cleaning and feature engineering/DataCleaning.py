b7ac14c66a5e88fe1ee1f12c79640c2ac68feeba
FSalinski <franek.salinski@gmail.com>
2022-11-23 22:26:33 +0100

    Cleaning02

--- Data cleaning and feature engineering/DataCleaning.py
+++ Data cleaning and feature engineering/DataCleaning.py
@@ -1,16 +1,33 @@
 import pandas as pd
 
-data = pd.read_csv('C:/Users/frane/kaggleproject/bank-data.csv', sep=';') # importing raw data
+data = pd.read_csv('./bank-data.csv', sep=';') # importing raw data
 
 #  Dropping columns we decided to drop in EDA
 
+print("Shape of data:", data.shape)
 data.drop(columns=['duration', 'default'], axis=1, inplace=True)
+print("*Columns dropped*\nNew shape of data:", data.shape)
 
 # Inputting missing data
 
 data['pdays'] = data['pdays'] < 999
-
 # pdays is True when a client was contacted in the previous campaign and False otherwise
+
 data.rename({'pdays':'contacted.in.previous'}, inplace=True)
 
-# Now we will get rid off 'unknown' values in categorical columns
+# Now we will get rid of 'unknown' values in categorical columns
+# How many records has any 'unknown' value?
+data_with_unknowns = data[(data['marital'] == 'unknown') |
+                            (data['housing'] == 'unknown') |
+                            (data['loan'] == 'unknown') |
+                            (data['job'] == 'unknown') ]
+print("Records with any 'unknown':", len(data_with_unknowns))
+print("Describtion of data with any unknowns:\n", data_with_unknowns.describe())
+
+# Dropping records with unknowns
+
+data.drop(axis=0, index=data_with_unknowns.index, inplace=True)
+
+# Exporting cleaned data to csv file
+
+data.to_csv('./Data cleaning and feature engineering/cleaned-bank-data.csv', index=False, sep=';')
---
+++ Data cleaning and feature engineering/FeatureEngineering.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+data = pd.read_csv('./Data cleaning and feature engineering/cleaned-bank-data.csv', sep=';')
+
+print(data)
