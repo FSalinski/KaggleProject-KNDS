1f70301bb769e8da877e60e0fb31ba79917ad2fd
FSalinski <franek.salinski@gmail.com>
2022-11-29 16:48:00 +0100

    FeatureEngineering02

--- Data_cleaning_and_feature_engineering/FeatureEngineering.py
+++ Data_cleaning_and_feature_engineering/FeatureEngineering.py
@@ -3,6 +3,9 @@ import numpy as np
 from sklearn import preprocessing
 import scipy.stats
 
+import warnings
+warnings.filterwarnings("ignore")
+
 def calculate_iqr(data, column):
     q1 = np.percentile(data[column], 25)
     q3 = np.percentile(data[column], 75)
@@ -22,7 +25,7 @@ def main():
     train_data = pd.read_csv('./Data/cleaned-train-bank-data.csv', sep=';')
     test_data = pd.read_csv('./Data/cleaned-test-bank-data.csv', sep=';')
 
-    numerical_to_drop_outliers = ['age','campaign','previous']
+    numerical_to_drop_outliers = ['age','campaign','previous','nr.employed']
 
     print("Shape of data: ", train_data.shape, test_data.shape)
     print("*Dropping records with outliers*")
@@ -36,24 +39,44 @@ def main():
     train_data.drop(columns=['previous'], axis=1, inplace=True)
     test_data.drop(columns=['previous'], axis=1, inplace=True)
     
-    # Checking skewness in data
-    # ...
+    features_to_scale = ['age','campaign','emp.var.rate','cons.price.idx','cons.conf.idx','euribor3m','nr.employed']
+
+    # Checking and removing skewness in data 
+
+    for column in features_to_scale:
+        print(f"\nSkewness in {column}: {scipy.stats.skew(train_data[column])}")
+
+    print("\n*Normalizing distributions*")
+
+    pt = preprocessing.PowerTransformer(method='yeo-johnson')
+
+    for column in features_to_scale:
+        train_data[column] = pt.fit_transform(train_data[[column]], pt.fit(train_data[[column]]))
+        test_data[column] = pt.fit_transform(test_data[[column]], pt.fit(train_data[[column]]))
+
+    for column in features_to_scale:
+        print(f"\nNew skewness in {column}: {scipy.stats.skew(train_data[column])}")
 
     # Scaling features to [0,1] range
 
+    print("\n*Scaling features*")
+
     scaler = preprocessing.MinMaxScaler()
-    
-    features_to_scale = ['age','campaign','emp.var.rate','cons.price.idx','cons.conf.idx','euribor3m','nr.employed']
     
     for column in features_to_scale:
         train_data[column] = scaler.fit_transform(train_data[[column]], scaler.fit(train_data[[column]]))
         test_data[column] = scaler.fit_transform(test_data[[column]], scaler.fit(train_data[[column]]))
     
-    print("*Checking data*")
+    print("\n*Checking data*")
     print("Train data description:\n", train_data.describe(), "\n")
     print("Train data head:\n", train_data.head(), "\n")
     print("Test data description:\n", test_data.describe(), "\n")
     print("Test data head:\n", test_data.head())
+
+    print("*Exporting data to csv files*")
+
+    train_data.to_csv('./Data/preprocessed-train-bank-data.csv', index=False, sep=';')
+    test_data.to_csv('./Data/preprocessed-test-bank-data.csv', index=False, sep=';')
       
 
 if __name__ == '__main__':
